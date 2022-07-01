271e7057d473ebccfbe92807c3a5da0eba70d29e
KLAIIIII <guyyg03546@gmail.com>
2022-07-01 16:41:06 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Hello.dart';
+import 'App.dart';
 
 void main(){
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
-  const MyApp({Key?key}):super(key: key); //1.constructor
 
-  @override //2.build
-  Widget build(BuildContext context){
-    //int _count = 0;
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HelloPage(),
-    );
-  }
-}
 
 
 
